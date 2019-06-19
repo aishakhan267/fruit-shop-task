6e26b243fa6b50ebe943646a8e8a40820483b412
aishakhan267 <49521671+aishakhan267@users.noreply.github.com>
2019-06-19 15:49:47 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ let orangeQty = 0;
 let bananaQty = 0;
 // i have 28 rupees total.example
 // Apple task
+
+function aon() {
+  document.querySelector(".two").style = " transform: scale(1.2); transition: 0.3s;";
+  document.querySelector(".three").style = " transform: scale(1.4); transition: 0.3s;";
+
+}
+function aout() {
+  document.querySelector(".two").style = " transform: scale(1); transition: 0.3s;";
+  document.querySelector(".three").style = " transform: scale(1); transition: 0.3s;";
+}
+
+
+
 function addApple() {
   let pocket = Number(document.querySelector("#all-about").value);
   let totalbill = Number(document.querySelector("#totalbill").innerHTML);
@@ -25,17 +38,26 @@ function minusApple() {
   document.querySelector('#totalbill').innerHTML = total;
 }
 // Orange task
+function oon() {
+  document.querySelector(".five").style = " transform: scale(1.2); transition: 0.3s;";
+  document.querySelector(".six").style = " transform: scale(1.4); transition: 0.3s;";
+
+}
+function oout() {
+  document.querySelector(".five").style = " transform: scale(1); transition: 0.3s;";
+  document.querySelector(".six").style = " transform: scale(1); transition: 0.3s;";
+}
 function addOrange() {
   let pocket = Number(document.querySelector("#all-about").value);
   let totalbill = Number(document.querySelector("#totalbill").innerHTML);
-  if (pocket <= document.querySelector("#totalbill").innerHTML){
+  if (pocket <= document.querySelector("#totalbill").innerHTML) {
     alert("Sorry! You can`t buy more");
-  }else{
+  } else {
     let addingOrange = document.querySelector('#orange').value = ++orangeQty;
-  console.log(addingOrange);
-  total = appleQty * 10 + orangeQty * 15 + bananaQty * 7;
-  document.querySelector('#totalbill').innerHTML = total;
- }
+    console.log(addingOrange);
+    total = appleQty * 10 + orangeQty * 15 + bananaQty * 7;
+    document.querySelector('#totalbill').innerHTML = total;
+  }
 }
 function minusOrange() {
 
@@ -45,17 +67,26 @@ function minusOrange() {
   document.querySelector('#totalbill').innerHTML = total;
 }
 // Banana task
+function bon() {
+  document.querySelector(".eight").style = " transform: scale(1.2); transition: 0.3s;";
+  document.querySelector(".nine").style = " transform: scale(1.4); transition: 0.3s;";
+
+}
+function bout() {
+  document.querySelector(".eight").style = " transform: scale(1); transition: 0.3s;";
+  document.querySelector(".nine").style = " transform: scale(1); transition: 0.3s;";
+}
 function addBanana() {
   let pocket = Number(document.querySelector("#all-about").value);
   let totalbill = Number(document.querySelector("#totalbill").innerHTML);
-  if (pocket <= document.querySelector("#totalbill").innerHTML){
+  if (pocket <= document.querySelector("#totalbill").innerHTML) {
     alert("Sorry! You can`t buy more");
-  }else{
-  let addingBanana = document.querySelector('#banana').value = ++bananaQty;
-  console.log(addingBanana);
-  total = appleQty * 10 + orangeQty * 15 + bananaQty * 7;
-  document.querySelector('#totalbill').innerHTML = total;
- }
+  } else {
+    let addingBanana = document.querySelector('#banana').value = ++bananaQty;
+    console.log(addingBanana);
+    total = appleQty * 10 + orangeQty * 15 + bananaQty * 7;
+    document.querySelector('#totalbill').innerHTML = total;
+  }
 }
 function minusBanana() {
   document.querySelector('#banana').value = --bananaQty;
